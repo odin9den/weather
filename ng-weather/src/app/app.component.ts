c0996a346c051426168b322e9759851f53438e2a
odin9den <odin9den@gmail.com>
2020-10-23 17:00:56 +0300

    created main look of the app / binded cities of the countries

--- ng-weather/src/app/app.component.ts
+++ ng-weather/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   appTitle = 'Angular Weather Application';
+  selectedCountry: any;
+  currentTemp: any;
+  condition: any;
+
+  cities = {};
+
+  countries = [
+    {
+      id: 1,
+      name: 'France',
+      cities: ['Paris', 'Marseille', 'Nice'],
+    },
+    {
+      id: 2,
+      name: 'Germany',
+      cities: ['Hamburg', 'Berlin', 'Munich'],
+    },
+    {
+      id: 3,
+      name: 'Italy',
+      cities: ['Roma', 'Milan', 'Napoli'],
+    },
+    {
+      id: 4,
+      name: 'Ukraine',
+      cities: ['Kyiv', 'Lviv', 'Dnipro'],
+    },
+  ];
+
+  ngOnInit() {
+    this.cities = this.countries.filter((x) => x.id == 1)[0].cities;
+  }
+
+  onChange(deviceValue) {
+    this.cities = this.countries.filter((x) => x.id == deviceValue)[0].cities;
+  }
 }
