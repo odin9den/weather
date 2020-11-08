cefcf7aa648a278eb6b027253c2cd1ec2656d989
odin9den <odin9den@gmail.com>
2020-11-08 17:59:10 +0200

    added city.json file

--- src/app/components/selection/selection.component.ts
+++ src/app/components/selection/selection.component.ts
@@ -2,7 +2,7 @@ import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { WeatherLoadResponse } from '../../models/weather-load-response';
 import { WeatherDataService } from '../../services/weatherdata.service';
 import { Weather } from './../../models/weather';
-import { default as jsonCityList } from './../../cities.json';
+import { default as jsonCityList } from './../../../app/';
 import { City } from 'src/app/models/city';
 
 @Component({
@@ -13,7 +13,7 @@ import { City } from 'src/app/models/city';
 export class SelectionComponent implements OnInit {
   @Output() onSelection: EventEmitter<Weather> = new EventEmitter<Weather>();
   weather: Weather = new Weather();
-  
+
   public selectedCity: string;
 
   public cityList: string[] = [];
@@ -22,21 +22,24 @@ export class SelectionComponent implements OnInit {
 
   ngOnInit(): void {
     const cities = <City[]>jsonCityList;
-    this.cityList = cities.filter(x => x.country == "UA").map(x => x.name).sort((one, two) => (one > two ? 1 : -1));
+    this.cityList = cities
+      .filter((x) => x.country == 'UA')
+      .map((x) => x.name)
+      .sort((one, two) => (one > two ? 1 : -1));
     this.selectedCity = null;
   }
 
   submit() {
     console.log(this.selectedCity);
-    this.weatherData.load(this.selectedCity).subscribe((data: WeatherLoadResponse) => {
-      this.weather.city = data.name;
-      this.weather.conditions = data.weather[0].main;
-      this.weather.temperature = Math.round(data.main.temp);
-      this.weather.icon = this.weatherData.getIconUrl(
-        data.weather[0].icon
-      );
+    this.weatherData
+      .load(this.selectedCity)
+      .subscribe((data: WeatherLoadResponse) => {
+        this.weather.city = data.name;
+        this.weather.conditions = data.weather[0].main;
+        this.weather.temperature = Math.round(data.main.temp);
+        this.weather.icon = this.weatherData.getIconUrl(data.weather[0].icon);
 
-      this.onSelection.emit(this.weather);
-    });
+        this.onSelection.emit(this.weather);
+      });
   }
 }
